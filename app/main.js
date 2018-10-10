18f2a208bed621c4c16ce86e9fa19dd3933886ec
frontlake <vladimirozerskiy@gmail.com>
2018-10-10 13:38:09 +0300

    Create a custom event and listen it in app component

--- app/main.js
+++ app/main.js
@@ -1,6 +1,23 @@
 const inputComponent = {
-  template: `<input class="input is-small" type="text" :placeholder="placeholder">`,
-  props: ['placeholder']
+  template: `<input class="input is-small" type="text" 
+              :placeholder="placeholder"
+              v-model="input"
+              @keyup.enter="monitorEnterKey">`,
+  props: ['placeholder'],
+  data() {
+    return {
+      input: ''
+    }
+  },
+  methods: {
+    monitorEnterKey() {
+      this.$emit('add-note', {
+        note: this.input,
+        timestamp: new Date().toLocaleString()
+      });
+      this.input = '';
+    }
+  }
 };
 
 new Vue({
@@ -12,5 +29,11 @@ new Vue({
   },
   components: {
     'input-component': inputComponent
+  },
+  methods: {
+    addNote(event) {
+      this.notes.push(event.note);
+      this.timestamps.push(event.timestamp);
+    }
   }
 })
